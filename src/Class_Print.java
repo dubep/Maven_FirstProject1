a6e3093a0156458a54f574b48c1a5a3fd59abeb4
dubep <premchand.dube@hpe.com>
2018-06-13 07:30:11 +0100

    This is commit first changes in my project

---
+++ src/Class_Print.java
@@ -0,0 +1,11 @@
+
+public class Class_Print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	System.out.println("This is my second project to test commit in GITHUB account");
+	}
+
+}
